05b45c8d5d7e2670340e539e7c095653a9e7f6a6
bayraktar1 <eggrep@hotmail.com>
2022-04-28 15:34:45 +0200

    extract removed transcripts to GTF

--- workflow/scripts/combine.py
+++ workflow/scripts/combine.py
@@ -6,7 +6,7 @@ Adds transcript counts to GTF files for FLAIR, TALON, and OXFORD.
 import logging
 
 # Setup logging
-logging.basicConfig(filename=snakemake.output.missing_transcripts,
+logging.basicConfig(filename=snakemake.output.removed_transcripts,
                     level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger = logging.getLogger(__name__)
@@ -75,7 +75,7 @@ def format_flair(abundance):
     return flair_dict
 
 
-def combine(gtf_file, counts, output):
+def combine(gtf_file, counts, output, missing_output):
     """
     Loops through the GTF file and checks the counts dictionary
     to add counts to the line and then writes it to a new file.
@@ -90,13 +90,18 @@ def combine(gtf_file, counts, output):
     stringtie for oxford. If there are any missing transcripts these
     will be written to the missing transcript log.
 
+    Transcripts with a count of 0 are filtered out.
+
+    :param missing_output: GTF file with the removed transcripts
     :param gtf_file: The GTF file produced by a pipeline.
     :param counts: Dictionary: key = transcript id, value = counts
     :param output: Output directory and file name
     :return: Writes a file to the output param
     """
     # Read the GTF file
-    with open(gtf_file) as GTF, open(output, 'w') as outfile:
+    with open(gtf_file) as GTF, open(output, 'w') as outfile, open(missing_output, 'a') as removed_out:
+        origin_file = gtf_file.split('/')[-1]
+        write = True
         for line in GTF:
             # Check if line is a transcript
             if line.split()[2] == 'transcript':
@@ -104,6 +109,12 @@ def combine(gtf_file, counts, output):
                     # Check for transcript id in counts
                     transcript_id = line.split()[11][1:-2]
                     count = counts[transcript_id]
+                    # if count is 0 transcript is removed
+                    if count == 0:
+                        logger.warning(f'ORIGIN: {origin_file} 0 count: {transcript_id}')
+                        removed_out.write(f'{line[:-1]}\n')
+                        write = False
+                        continue
                 except KeyError:
                     # Transcript is missing from the abundance file
                     try:
@@ -114,15 +125,24 @@ def combine(gtf_file, counts, output):
                     except KeyError:
                         # transcript is actually missing
                         # Add missing transcript id to missing transcripts.log
-                        origin_file = gtf_file.split('/')[-1]
                         logger.error(f'ORIGIN: {origin_file} FIRST TRY: {transcript_id} SECOND TRY: {new_transcript_id}')
-                        count = 0
+                        removed_out.write(f'{line[:-1]}\n')
+                        write = False
+                        continue
+
+                # transcript is not missing from the abundance and has at least 1 count.
                 # Add transcript count to end of line and write to file
                 # counts are not in TPM, hijacking the column for use with GFFcompare
+                write = True
                 outfile.write(f'{line[:-1]} TPM "{str(count)}";\n')
             else:
                 # Line is not a transcript
-                outfile.write(line)
+                if write:
+                    # Line is not an exon from a removed transcript
+                    outfile.write(line)
+                else:
+                    # Line is an exon from a removed transcript
+                    removed_out.write(line)
 
 
 def main():
@@ -135,20 +155,19 @@ def main():
     flair_abundance = snakemake.input.flair_count
     flair_gtf = snakemake.input.flair_transcripts
     flair_dict = format_flair(flair_abundance)
-    combine(flair_gtf, flair_dict, snakemake.output.flair_combined)
+    combine(flair_gtf, flair_dict, snakemake.output.flair_combined, snakemake.output.flair_removed)
     flair_dict.clear()
 
     ox_abundance = snakemake.input.oxford_count
     ox_gtf = snakemake.input.oxford_transcript
     ox_dict = format_oxford(ox_abundance)
-    combine(ox_gtf, ox_dict, snakemake.output.oxford_combined)
+    combine(ox_gtf, ox_dict, snakemake.output.oxford_combined, snakemake.output.oxford_removed)
     ox_dict.clear()
 
     talon_abundance = snakemake.input.talon_count
     talon_gtf = snakemake.input.talon_transcripts
     talon_dict = format_talon(talon_abundance)
-    combine(talon_gtf, talon_dict, snakemake.output.talon_combined)
-    talon_dict.clear()
+    combine(talon_gtf, talon_dict, snakemake.output.talon_combined, snakemake.output.talon_removed)
 
 
 main()
