a7069ea8c238a15edbb4ad886ef70be2d075b668
DavyCats <davycats.dc@gmail.com>
2025-04-24 14:30:29 +0200

    update resources

--- run.sh
+++ run.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
-#
-#SBATCH --job-name="RPCA"
-#SBATCH --mem=100g
-#SBATCH --ntasks-per-node=10
-#SBATCH --time=24:0:0
-#SBATCH --partition=all,highmem
-#SBATCH --output=RPCA-%j.out
-#SBATCH --mail-user="d.bayraktar@lumc.nl"
-#SBATCH --mail-type=END
+
+set -e
 
 CONF="config/config.yaml"
 
-# Create rulegraph
-#snakemake -n --dry-run -p --use-singularity --singularity-prefix "resources"  --singularity-args "--bind /exports" --use-conda -j 10 all --configfile $CONF --rulegraph | dot -Tpdf > rulegraph.pdf
-
-# Create Snakemake report (after creating all results)
-#snakemake -p --use-singularity --singularity-prefix "resources"  --singularity-args "--bind /exports" --use-conda -j 10 all --report report.html --configfile $CONF
-
-# Run the pipeline
-snakemake -p --use-singularity --singularity-prefix "resources" --singularity-args "--bind /exports" --use-conda -j 10 all --configfile $CONF
-
-# Dryrun and state reason for running rules
-#snakemake -n --reason --dry-run -p --use-singularity --singularity-prefix "resources"  --singularity-args "--bind /exports" --use-conda -j 1 all --configfile $CONF
+# Run the pipeline using slurm
+snakemake -p --profile slurm --use-singularity --singularity-prefix "resources" --singularity-args "--bind /exports" all --configfile $CONF
